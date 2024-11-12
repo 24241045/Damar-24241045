1d02b8cdf9b0311fd0beeb3228a67c7203897ccf
24241045 <aldamarkaromi@gmail.com>
2024-11-12 11:44:49 +0800

    Add files via upload

---
+++ Damar_ALGO/01-Variabel/main.py
@@ -0,0 +1,25 @@
+# Variabel adalah tempat menyimpan data
+
+# Variabel data / assigment data
+a = 10 
+x = 5
+panjang = 1000
+
+# memanggil variabel 
+print("Nilai a : ", a)
+print("Nilai x : ", x)
+print("Nilai panjang : ", panjang)
+
+# Penanaman variabel 
+nilai_y =15 # dengan underscore
+jura10 = 1000000000 # boleh dipake
+nilaiangka = 100 # model camel
+
+# mengubah isi variabel 
+print("nilai a : ", a) # nilai awal = 10
+a = 7
+print("nilai a : ",) # nilai a di unah = 7
+
+print("nilai x : ", a) # nilai x awal = 5
+x = a
+print("nilai x : ", a) # nilai x diubah - 7
---
+++ Damar_ALGO/02-Tipe Data/main.py
@@ -0,0 +1,26 @@
+# a - 10, a adalah variabel yang nilainya 10
+
+# integer
+# tipe data : angka satuan yang tidak punya koma
+data_integer = 11
+print("data : ", data_integer)
+# type() berfungsi untuk mengetahui tipe data variabel
+print("tipe data : ", type (data_integer)) # <class ' int
+
+# float 
+# tipe data : angka dengan koma
+dara_float = 3.14
+print("data : ",dara_float)
+print("data : ", type(dara_float))
+
+# string
+# tipe data : kumpulan karakter 
+data_string = "Universitas pendidikan mandalika"
+print("data : ", data_string)
+print("data : ", type (data_string))
+
+# boolean
+# tipe data : true / false
+data_boolean = False
+print("data : ", data_boolean)
+print("data : ", type (data_boolean))
